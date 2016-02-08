5f3776261e148eb528055ded593b622cd848afaa
Nikita <nik.v.gil@gmail.com>
2016-02-08 20:49:08 +0200

    Initial Commit

---
+++ RubyGarageIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RubyGarageIOS
+//
+//  Created by Nikita Gil' on 08.02.16.
+//  Copyright © 2016 Nikita Gil'. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
